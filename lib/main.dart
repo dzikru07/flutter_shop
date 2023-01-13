43954d0de379ec1a15c50e0a3f9864d8d2bd5d8e
dzikru07 <dzikru07@gmail.com>
2023-01-13 15:29:33 +0700

    fitst commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'component/bottom_bar.dart';
+import 'style/color.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Shop',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: colorTheme,
+      ),
+      home: const NavigationBarComponen(),
+    );
+  }
+}
---
+++ lib/style/color.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+Map<int, Color> redTheme = {
+  50: const Color(0xffFF3636),
+  100: const Color(0xffFF3636),
+  200: const Color(0xffFF3636),
+  300: const Color(0xffFF3636),
+  400: const Color(0xffFF3636),
+  500: const Color(0xffFF3636),
+  600: const Color(0xffFF3636),
+  700: const Color(0xffFF3636),
+  800: const Color(0xffFF3636),
+  900: const Color(0xffFF3636),
+};
+
+MaterialColor colorTheme = MaterialColor(0xffFF3636, redTheme);
+
+Color mainColor = Color(0xffFF3636);
